b60a655e0a4f6b032053cb65e5b1154fdd495a30
Leeforgiveness <grang0224@g.kmou.ac.kr>
2024-09-23 23:19:21 +0900

    0923

---
+++ java/120875/src/Main.java
@@ -0,0 +1,34 @@
+//dots
+// 1,2 3,4
+// 1,3 2,4
+// 1,4 2,3
+
+class Solution {
+    public static int solution(int[][] dots) {
+        int answer = 0;
+
+        float t1 = (dots[0][1] - dots[1][1]) / (dots[0][0] - dots[1][0]);
+        float t2 = (dots[2][1] - dots[3][1]) / (dots[2][0] - dots[3][0]);
+        if(t1 == t2)
+            return 1;
+
+        t1 = (dots[0][1] - dots[2][1]) / (dots[0][0] - dots[2][0]);
+        t2 = (dots[1][1] - dots[3][1]) / (dots[1][0] - dots[3][0]);
+        if(t1 == t2)
+            return 1;
+
+        t1 = (dots[0][1] - dots[3][1]) / (dots[0][0] - dots[3][0]);
+        t2 = (dots[1][1] - dots[2][1]) / (dots[1][0] - dots[2][0]);
+        if(t1 == t2)
+            return 1;
+
+        return answer;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution(new int[][] {{1, 4}, {9, 2}, {3, 8}, {11, 6}}));
+        System.out.println(Solution.solution(new int[][] {{3, 5}, {4, 1}, {2, 4}, {5, 10}}));
+    }
+}
