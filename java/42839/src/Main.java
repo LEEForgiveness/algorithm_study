18b03cf109b09c90ac8a42591528f1dec1cef21b
Leeforgiveness <grang0224@g.kmou.ac.kr>
2025-05-30 01:03:06 +0900

    25.05.30

---
+++ java/42839/src/Main.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    public static int solution(String numbers) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Solution.solution("17"));
+    }
+}
