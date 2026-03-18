9adf0cc4d3154a7113eed6f178625d79293714a9
Leeforgiveness <grang0224@g.kmou.ac.kr>
2026-03-18 15:33:04 +0900

    260318 벽 부수고 이동하기(2206)

--- java/260318/src/Main.java
+++ java/260318/src/Main.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main2 {
+public class Main {
     static int n, m;
     static int[][] map;
     public static void main(String[] args) throws IOException {
@@ -22,16 +22,14 @@ public class Main2 {
         }
         bfs();
     }
-
     static int[] dr = {0, 1, 0, -1};
-    static int[] dc = {1, 0 , -1, 0};
-    static void bfs(){
+    static int[] dc = {1, 0, -1, 0};
+    static void bfs() {
         int[][][] dist = new int[n][m][2];
-        dist[0][0][0] = 1;
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0, 0, 0});
-
-        while(!queue.isEmpty()){
+        queue.add(new int[] {0, 0, 0});
+        dist[0][0][0] = 1;
+        while (!queue.isEmpty()){
             int[] cur = queue.poll();
             int curR = cur[0];
             int curC = cur[1];
@@ -39,26 +37,31 @@ public class Main2 {
             for (int i = 0; i < 4; i++) {
                 int nextR = curR + dr[i];
                 int nextC = curC + dc[i];
-                if (0 <= nextC && nextC < m && 0 <= nextR && nextR < n && map[nextR][nextC] == 0 && dist[nextR][nextC][broken] == 0) {
-                    queue.add(new int[] {nextR, nextC, broken});
-                    dist[nextR][nextC][broken] = dist[curR][curC][broken] + 1;
+                if (nextR < 0 || nextR >= n || nextC < 0 || nextC >= m) {
+                    continue;
                 }
 
-                if (0 <= nextC && nextC < m && 0 <= nextR && nextR < n && map[nextR][nextC] == 1 && broken == 0 && dist[nextR][nextC][1] == 0) {
-                    queue.add(new int[] {nextR, nextC, 1});
+                if (map[nextR][nextC] == 0 && dist[nextR][nextC][broken] == 0) {
+                    dist[nextR][nextC][broken] = dist[curR][curC][broken] + 1;
+                    queue.add(new int[] {nextR, nextC, broken});
+                }
+
+                if (map[nextR][nextC] == 1 && broken == 0 && dist[nextR][nextC][broken] == 0) {
                     dist[nextR][nextC][1] = dist[curR][curC][0] + 1;
+                    queue.add(new int[] {nextR, nextC, 1});
                 }
             }
         }
-
-        if (dist[n - 1][m - 1][0] == 0 && dist[n - 1][m - 1][1] == 0) {
+        int a = dist[n - 1][m - 1][0];
+        int b = dist[n - 1][m - 1][1];
+        if (a == 0 && b == 0) {
             System.out.println(-1);
-        } else if (dist[n - 1][m - 1][0] == 0) {
-            System.out.println(dist[n - 1][m - 1][1]);
-        } else if (dist[n - 1][m - 1][1] == 0) {
-            System.out.println(dist[n - 1][m - 1][0]);
+        } else if (a == 0) {
+            System.out.println(b);
+        } else if (b == 0) {
+            System.out.println(a);
         } else {
-            System.out.println(Math.min(dist[n - 1][m - 1][0], dist[n - 1][m - 1][1]));
+            System.out.println(Math.min(a, b));
         }
     }
 }
